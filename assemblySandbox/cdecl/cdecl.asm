654540c896a506075e028948ff66fa7ea642d93b
Vert Simon <stopmotion45b@gmail.com>
2022-04-03 21:52:38 -0400

    More assembly features explored. I am starting to really like assembly!

--- assemblySandbox/cdecl/cdecl.asm
+++ assemblySandbox/cdecl/cdecl.asm
@@ -4,8 +4,17 @@ section .data
 
 section .bss
     arg1: resd 1
+    arg2: resd 1
 section .text
 
 fpuTest:
+    ;this function will mov the values of xmm0 and xmm1 to the fpus
+    movss [rel arg1], xmm0
+    movss [rel arg2], xmm1
+    fld dword [rel arg1]
+    fld dword [rel arg2]
+    
     fstp dword [rel arg1]
+    fstp dword [rel arg2]
+
     ret
--- assemblySandbox/stackTest.asm
+++ assemblySandbox/stackTest.asm
@@ -17,12 +17,20 @@ stackTest:
 	lea rax, [rel increment]
 	movups xmm1, [rax]
 	addps xmm0, xmm1
+	
+	lea rax, [rel temp]
+
+	movss xmm4, xmm0
+	movss [rax], xmm4
+	fld dword [rax]
+
 	leave
 	ret
 
 section .bss 
 	
 	result: resb 128
+	temp: resb 32
 
 section .data
 	increment: dd 1.0, 1.0, 1.0, 1.0
